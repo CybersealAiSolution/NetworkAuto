dcd36f2c086ddafd7c43220b334cc86bbd1bc899
yogesh-cybersealai <yogesh@cybersealai.com>
2023-08-09 14:48:07 +0530

    tabs for switches,access points,subnets are added in AddressDetailView page

---
+++ src/Fetch.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const getCookie = (name) => {
+  const cookieValue = document.cookie.match(
+    "(^|;)\\s*" + name + "\\s*=\\s*([^;]+)"
+  );
+  return cookieValue ? cookieValue.pop() : "";
+};
+const csrftoken = getCookie("csrftoken");
+// Set the CSRF token in the request headers
+axios.defaults.headers.post["X-CSRFToken"] = csrftoken;
+
+export const instance = axios.create({
+  baseURL: "http://localhost:5000",
+  withCredentials: true, // This ensures cookies (sessions) are sent with every request
+});
--- src/components/CreateDefaultTanent/CreateDefaultTenant.js
+++ src/components/CreateDefaultTanent/CreateDefaultTenant.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./index.css";
 import axios from "axios";
-import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
 
 const CreateDefaultTenant = () => {

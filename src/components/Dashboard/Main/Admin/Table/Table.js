ad587e7764436032aa8e29702edf1a162c169a0d
yogesh-cybersealai <yogesh@cybersealai.com>
2023-08-09 21:46:38 +0530

    add admin form functionality added

--- src/components/Dashboard/Main/Admin/Table/Table.js
+++ src/components/Dashboard/Main/Admin/Table/Table.js
@@ -1,117 +1,122 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
-import axios from "axios";
 import "./index.css";
+import { instance } from "../../../../../Fetch";
 // import { Link } from "react-router-dom";
 
 const TableComponent = () => {
   // const [error, setError] = useState(null);
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [adminEmail, setAdminEmail] = useState("");
+const [accessLevel, setAccessLevel] = useState("FullAccess");
+
   const sidebarRef = useRef(null);
-  const [newAdminEmail,setNewAdminEmail] = useState("");
-  const [data, setData] = useState([
-    {
-      Admin: "CSA_Global@MODERNCOMMS450672.onmicrosoft.com",
-      AccessLevel: "Full Access",
-    },
-    
-  ]);
-  const getCookie = (name) => {
-    const cookieValue = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
-    return cookieValue ? cookieValue.pop() : '';
-  }
-  const csrftoken = getCookie('csrftoken');
-  // Set the CSRF token in the request headers
-  axios.defaults.headers.post["X-CSRFToken"] = csrftoken;
+  const [data, setData] = useState([]);
+  useEffect(() => {
 
-  const instance = axios.create({
-    baseURL: "http://localhost:5000",
-    withCredentials: true, // This ensures cookies (sessions) are sent with every request
-  });
-//   const route = Ro
-  useEffect(()=>{
-    // document.addEventListener('click', handleClickOutside);
-        
-
-    const getEmergencyAddress = async () => {
+    const getAllAdmins = async () => {
       try {
-        const response = await instance.get("/getEmergencyAddresses", {
-          headers: {
-            "Content-Type": "application/json",
-            'X-CSRFToken': getCookie('csrftoken'),
-          },
-        });
-    
-        
-        console.log("s", response.data);
+        const response = await instance.get("/getalladmins");
+
+        // console.log("getAllAdmins", response.data);
+        setData(response.data.data ? response.data.data : []);
         if (response.data.error) {
           alert(response.data.error);
           return;
         } else {
           // navigate("/");
-          console.log("first");
+          console.log("getalladmins success");
         }
       } catch (error) {
         // Handle any errors that may occur during the API call
         console.error("Error sending data:", error);
       }
-      }
-    getEmergencyAddress();
+    };
+    getAllAdmins();
+  }, []);
 
-    // return () => {
-    //   document.removeEventListener('click', handleClickOutside);
-    //   };
+  const submitForm = async (event) => {
+    event.preventDefault();
+    console.log("Admin Email:", adminEmail);
+  console.log("Access Level:", accessLevel);
+  const payload = {
+    'userName':adminEmail,
+    'roles':accessLevel
+  }
+  const response = await instance.post("/addAdmin",payload);
+  };
 
-  },[]);
-
-
-  const submitForm = event => {
-    // event.prevent
-
-};
-
- 
   const TableColumn = () =>
     data.map((item) => (
       <tr key={item.id}>
-        <td><input className="rowCheckbox" type="checkbox"></input></td>
-        <td>{item.Admin}</td>
-        <td>{item.AccessLevel}</td>
+        <td>
+          <input className="rowCheckbox" type="checkbox"></input>
+        </td>
+        <td>{item.userName}</td>
+        <td>{item.roles[0]}</td>
       </tr>
     ));
 
   return (
     <div className="tableComponent">
       <div className="tableHeader">
-
         {/* <Link className="addbtn" to="/dashboard/add-address">+ Add</Link> */}
-        <div onClick={() => setSidebarOpen(!isSidebarOpen)} className="addbtn">+ add</div>
-        {isSidebarOpen && 
-              <>
-                <div className="overlay"></div>
-                <div className="sidebar2" ref={sidebarRef}>
-                    <div className="closeSidebar2" onClick={() => setSidebarOpen(!isSidebarOpen)} >X</div>
-                    <h2>Add Admin</h2>
-                    <form className="addUserForm">
-                      <div>
-                      <div className="adminEmailFormDivision adminFormElement">
-                        <label for="adminEmail">Email</label>
-                        <input type="text" name="adminEmail" className="adminEmail" ></input>
-                      </div>
-                      <div className="AccessLevelFormDivision adminFormElement">
-                        <label for="accessLevel">Access level</label>
-                        <select type="text" name="accessLevel" className="accessLevel" >
-                          <option id="FullAccess" value="FullAccess">Full Access</option>
-                          <option id="ReadOnly" value="ReadOnly">Read Only</option>
-                          <option id="ReadAndWrite" value="ReadAndWrite">Read And Write</option>
-                        </select>
-                      </div>
-                      </div>
-                      <input type="submit" className="addAdminFormSubmit" onClick={()=>{submitForm()}}></input>
-                    </form>
+        <div onClick={() => setSidebarOpen(!isSidebarOpen)} className="addbtn">
+          + add
+        </div>
+        {isSidebarOpen && (
+          <>
+            <div className="overlay"></div>
+            <div className="sidebar2" ref={sidebarRef}>
+              <div
+                className="closeSidebar2"
+                onClick={() => setSidebarOpen(!isSidebarOpen)}
+              >
+                X
+              </div>
+              <h2>Add Admin</h2>
+              <form className="addUserForm" onSubmit={submitForm}>
+                <div>
+                  <div className="adminEmailFormDivision adminFormElement">
+                    <label for="adminEmail">Email</label>
+                    <input
+                      type="email"
+                      name="adminEmail"
+                      className="adminEmail"
+                      value={adminEmail}
+                      onChange={(e) => setAdminEmail(e.target.value)}
+                    />
+                  </div>
+                  <div className="AccessLevelFormDivision adminFormElement">
+                    <label for="accessLevel">Access level</label>
+                    <select
+                      type="text"
+                      name="accessLevel"
+                      className="accessLevel"
+                      value={accessLevel}
+                      onChange={(e) => setAccessLevel(e.target.value)}
+                    >
+                      <option id="FullAccess" value="admin">
+                        Full Access
+                      </option>
+                      <option id="ReadOnly" value="ReadOnly">
+                        Read Only
+                      </option>
+                      <option id="ReadAndWrite" value="ReadAndWrite">
+                        Read And Write
+                      </option>
+                    </select>
+                  </div>
                 </div>
-              </>
-            }
+                <input
+                  type="submit"
+                  className="addAdminFormSubmit"
+                  value="Submit"
+                ></input>
+              </form>
+            </div>
+          </>
+        )}
 
         <div className="tableSearchContainer">
           <input
@@ -125,7 +130,9 @@ const TableComponent = () => {
         <table className="table">
           <thead>
             <tr>
-            <th><input className="headerCheckbox" type="checkbox"></input></th>
+              <th>
+                <input className="headerCheckbox" type="checkbox"></input>
+              </th>
               <th>Admin</th>
               <th>Access Level</th>
             </tr>
--- src/components/Dashboard/Main/Devices/Table/Table.js
+++ src/components/Dashboard/Main/Devices/Table/Table.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./index.css";  // Assuming you want to use the same CSS
 
 const DeviceTableComponent = () => {

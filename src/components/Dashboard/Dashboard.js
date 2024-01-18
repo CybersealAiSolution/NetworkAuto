32d14340cababb3015fa8e34438c69e86a1d58ad
kumar satyam adarsh <satyama@cybersealai.com>
2024-01-18 17:36:46 +0530

    fixed UI

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,14 +8,16 @@ import { width } from "@mui/system";
 const Dashboard = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-    // Inline styles for dynamic width adjustment
+    // Inline styles for dynamic width adjustment with transition
     const headerAndMainStyle = {
         width: isSidebarOpen ? '84%' : `calc(100% - 90px)`,
         minWidth: isSidebarOpen ? `calc(100% - 280px)` : `calc(100% - 90px)`,
+        transition: 'width 0.5s ease-in-out'  // Corrected property name
     };
     const sidebarStyle = {
         width: isSidebarOpen ? '16%' : '90px',
         minWidth: isSidebarOpen ? '280px' : '90px',
+        transition: 'width 0.5s ease-in-out'  // Corrected property name
     };
 
     return (
